d49cf9092af63dff5731fc0e1e99fe30840700bc
jarltuxen <jarl.tuxen@gmail.com>
2022-03-17 11:21:53 +0100

    insertRow

--- src/JdbcTest.java
+++ src/JdbcTest.java
@@ -9,12 +9,18 @@ public class JdbcTest {
     private static Connection connection;
 
     public static void main(String[] args) {
+        getAll();
+        insertRow();
+        getAll();
+    }
+
+    public static void getAll(){
         //create connection
         Connection con = getConnection();
         try{
 
             Statement s = con.createStatement();
-            final String SQL_QUERY = "SELECT * FROM product";
+            final String SQL_QUERY = "SELECT * FROM product"; //" WHERE id = 1 OR 1=1; --";
             ResultSet rs = s.executeQuery(SQL_QUERY);
 
             //read data from resultset
@@ -26,16 +32,32 @@ public class JdbcTest {
             }
 
             s.close();
-
-
         }
         catch (SQLException e){
             System.out.println("Could not create connection");
             e.printStackTrace();
         }
     }
+
+    public static void insertRow(){
+        final String INSERT_SQL ="INSERT INTO product(name, price) VALUES (?, ?)";
+        connection = getConnection();
+        try {
+            PreparedStatement psInsertProduct = connection.prepareStatement(INSERT_SQL);
+            psInsertProduct.setString(1, "Slikkepind");
+            psInsertProduct.setInt(2, 10);
+            psInsertProduct.executeUpdate();
+            System.out.println("Product inserted");
+        }
+        catch (SQLException e){
+            System.out.println("Could not insert row");
+            e.printStackTrace();
+        }
+
+    }
     
     public static Connection getConnection(){
+        //connection er en singleton
         //connection already initialized?
         if (connection!=null) return connection;
 
